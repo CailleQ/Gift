aad3c8f8b60782fedef6cea9f9a4bb3e84dc4e72
MA Haoran <m2954687470@gmail.com>
2024-04-07 00:38:13 +0200

    提交一下周五的尾巴
    周六凌晨0点36分,
    自己一个人听歌看电锯人,
    总有种惨惨的感觉，想跟哈导一起看,
    但他去跟朋友打电话去了,掉小珍珠

--- Levels/Script/world.gd
+++ Levels/Script/world.gd
@@ -4,5 +4,10 @@ func _process(delta: float) -> void:
 
 func rotate_sky() -> void:
 	var world_environment = $WorldEnvironment.get_environment()
+	var world_Light = $DirectionalLight3D
 	if world_environment != null:
 		world_environment.sky_rotation.y += 0.0005
+		
+	#尝试 光源的旋转
+	#if world_Light != null:
+		#world_Light.y += 0.0005
