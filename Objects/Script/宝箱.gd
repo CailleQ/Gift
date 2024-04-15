1c883c3c2172c18923dc34128cfc34fcdbf11c96
MA Haoran <m2954687470@gmail.com>
2024-04-15 15:22:43 +0200

    还剩宝箱对话/逻辑，文件写入 以及bgm的添加与操作
    可能ui还要修改一下，将菜单界面植入游戏界面，未开始游戏前不监听 键盘任何动作

--- Objects/Script/宝箱.gd
+++ Objects/Script/宝箱.gd
@@ -1,19 +1,61 @@
 extends Sprite3D
+
 @onready var actionable = get_node("./Actionable")
 
 func _ready():
-	switch_dialog()
 	
+	#初始化 对话脚本开始位置
+	switch_dialog()
+
+
+@warning_ignore("unused_parameter")
+func _physics_process(delta):
+	
+	#从state.gd中调取can_open_ 参数进行状态判断
+	var can_open_A = get_node("/root/State").can_open_A
+	var can_open_B = get_node("/root/State").can_open_B
+
+	#分别控制宝箱A与B的开启
+	if name == "宝箱A" and can_open_A == 1:
+		frame = 1
+	if name == "宝箱B" and can_open_B == 1:
+		frame = 1
+		add_file(happybirthday)
+
 
 func operat():
-	#处理碰撞函数
-	#修改be_opened值
-	frame = 1
-	pass
-	
+	var count = get_node("/root/State").count
+	print(count) #测试 count 数值变动
+
+
 func switch_dialog() :
 	print(name)
 	if name == "宝箱A":
 		actionable.dialogue_start = "宝箱A"
 	else:
 		actionable.dialogue_start = "宝箱B"
+
+#本地添加/修改txt文件
+#func add_file(content):
+	#var file_path = "user://%appdata%/godot/app_userdata/gift.txt"
+	##%appdata%/godot/app_userdata
+	#var file = FileAccess.open("user://%appdata%/godot/app_userdata/gift.txt", FileAccess.WRITE)
+	#if file != null:
+		#file.store_string(content)
+	#else : 
+		#print("can not open file")
+
+#未成功 待修改
+func add_file(content, file_path = "user://%appdata%/godot/app_userdata/gift.txt"):
+	var file = FileAccess.open(file_path, FileAccess.WRITE )  # Open for writing and create if needed
+	if not file:
+		print("Error creating file:", file_path)
+		return
+	file.store_string(content)
+	file.close()
+
+
+#想写的话
+const happybirthday = " 
+阿巴阿巴阿巴阿巴阿巴阿巴
+"
--- State/state.gd
+++ State/state.gd
@@ -1,8 +1,10 @@
 extends Node
 
 #宝箱的states
-@export var cheast_status = 0
-@export var cleaed = 0
+@export var can_open_A = 0
+@export var can_open_B = 1
+@export var cleared = 0
+@export var count = 0
 #门的states
 @export var door_status = 0
 @export var door_words = 0
